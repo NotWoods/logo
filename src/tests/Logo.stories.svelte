ba7f673249e3b1b142ce4538abe58b708f2b4c1c
Tiger Oakes <contact@tigeroakes.com>
2024-04-23 21:53:25 -0700

    Fix formatting

--- src/tests/Logo.stories.svelte
+++ src/tests/Logo.stories.svelte
@@ -1,54 +1,54 @@
 <script context="module">
-  import Logo from "../lib/Logo.svelte";
+	import Logo from '../lib/Logo.svelte';
 
-  /** @type {import('@storybook/svelte').Meta<Logo>} */
-  export const meta = {
-    component: Logo,
-    args: {
-      monochrome: false,
-      height: 32,
-      animationOn: "mount"
-    },
-    argTypes: {
-      color: {
-        control: 'color'
-      },
-      animationOn: {
-        control: 'radio',
-        options: ['mount', 'hover', 'none']
-      }
-    }
-  };
+	/** @type {import('@storybook/svelte').Meta<Logo>} */
+	export const meta = {
+		component: Logo,
+		args: {
+			monochrome: false,
+			height: 32,
+			animationOn: 'mount'
+		},
+		argTypes: {
+			color: {
+				control: 'color'
+			},
+			animationOn: {
+				control: 'radio',
+				options: ['mount', 'hover', 'none']
+			}
+		}
+	};
 </script>
 
 <script>
-  import { Story, Template } from "@storybook/addon-svelte-csf";
+	import { Story, Template } from '@storybook/addon-svelte-csf';
 </script>
 
 <Template let:args>
-  <div class:anim--hover={args.animationOn === "hover"}>
-    <Logo {...args} />
-  </div>
+	<div class:anim--hover={args.animationOn === 'hover'}>
+		<Logo {...args} />
+	</div>
 </Template>
 
 <Story name="Full Color" args={{ monochrome: false }} />
 <Story name="Monochrome" args={{ monochrome: true }} />
-<Story name="Tinted" args={{ monochrome: true, color: "red" }} />
+<Story name="Tinted" args={{ monochrome: true, color: 'red' }} />
 <Story
-  name="Dark"
-  args={{ monochrome: false, color: 'white' }}
-  parameters={{ backgrounds: { default: "dark" } }}
+	name="Dark"
+	args={{ monochrome: false, color: 'white' }}
+	parameters={{ backgrounds: { default: 'dark' } }}
 />
 <Story
-  name="Dark Monochrome"
-  args={{ monochrome: true, color: 'white' }}
-  parameters={{ backgrounds: { default: "dark" } }}
+	name="Dark Monochrome"
+	args={{ monochrome: true, color: 'white' }}
+	parameters={{ backgrounds: { default: 'dark' } }}
 />
-<Story name="Animate on hover" args={{ animationOn: "hover" }} />
-<Story name="No Animation" args={{ animationOn: "none" }} />
+<Story name="Animate on hover" args={{ animationOn: 'hover' }} />
+<Story name="No Animation" args={{ animationOn: 'none' }} />
 
 <style>
-  .anim--hover:hover {
-    --logo-animation-progress: 1;
-  }
+	.anim--hover:hover {
+		--logo-animation-progress: 1;
+	}
 </style>
