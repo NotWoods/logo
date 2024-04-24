70c5998951754e710bbcb9aee4bfeb8cfc127e77
Tiger Oakes <contact@tigeroakes.com>
2024-04-23 21:42:37 -0700

    Add brand white color

--- src/routes/[file].svg/presets.js
+++ src/routes/[file].svg/presets.js
@@ -1,7 +1,7 @@
 export const LOGO_PRESETS = /** @type {const} */ ({
 	logo: { monochrome: false },
 	'logo-monochrome': { monochrome: true },
-	'logo-on-dark': { color: 'white' },
+	'logo-on-dark': { color: '#ebeeef' },
 	'logo-on-dark-monochrome': { color: 'white', monochrome: true },
 	'logo-animate-on-hover': { animationOn: 'hover' },
 	'logo-static': { animationOn: 'none' },
